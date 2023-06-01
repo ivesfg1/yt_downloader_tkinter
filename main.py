801bedd2bec32cf17bbf42f2823906b90a77321b
ivesfg1 <wigzfurtado@gmail.com>
2023-06-01 01:17:04 -0300

    renamed few methods, but BASE_DIR inside main class and removed unused hastags

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from pytube import YouTube
 from pathlib import Path
-
-BASE_DIR = Path(__file__).parent / "downloads"
+from pytube import YouTube
 
 
 class YoutubeDownloader:
+    """
+    Main class
+    """
+
+    BASE_DIR = Path(__file__).parent / "downloads"
+
     @classmethod
     def generate_filename(cls, video_title, file_extension, output_path):
-        #
         file_extension = file_extension.lower()
         filename = f"{video_title}.{file_extension}"
 
@@ -20,9 +23,8 @@ class YoutubeDownloader:
 
     @classmethod
     def download_video(cls, video_url, file_extension="mp4"):
-        #
         youtube_video = YouTube(video_url)
-        output_path = BASE_DIR / "videos"
+        output_path = cls.BASE_DIR / "videos"
 
         video_title = youtube_video.title
         filename = cls.generate_filename(video_title, file_extension, output_path)
@@ -33,5 +35,5 @@ class YoutubeDownloader:
 
 
 if __name__ == "__main__":
-    video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    YoutubeDownloader.download_video(video_url)
+    url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"  # Rick Astley's classic :P
+    YoutubeDownloader.download_video(url)
